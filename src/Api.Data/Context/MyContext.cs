e5e5aa4f5ddf84b80bffcf71e9ed3abfa2743ec5
Andre <9000andre@gmail.com>
2020-11-14 11:18:31 -0300

    Create folde in Api.Data for configure UF model.build - configure MyContext in Api.Data

--- src/Api.Data/Context/MyContext.cs
+++ src/Api.Data/Context/MyContext.cs
@@ -2,6 +2,10 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Api.Domain.Entities;
 using Api.Data.Mapping;
+using Api.Data.Mapping.Cep;
+using Api.Data.Mapping.Municipio;
+using Api.Data.Mapping.Uf;
+using Api.Data.Seeds;
 namespace Api.Data.Context
 {
     public class MyContext : DbContext
@@ -14,6 +18,9 @@ namespace Api.Data.Context
            base.OnModelCreating(modelBuilder);
 
            modelBuilder.Entity<UserEntity>(new UserMap().Configure);
+           modelBuilder.Entity<UfEntity>(new UfMap().Configure);
+           modelBuilder.Entity<MunicipioEntity>(new MunicipioMap().Configure);
+           modelBuilder.Entity<CepEntity>(new CepMap().Configure);
 
             //add user default
             modelBuilder.Entity<UserEntity>().HasData(
@@ -25,7 +32,9 @@ namespace Api.Data.Context
                     CreateAt = DateTime.UtcNow,
                     UpdateAt = DateTime.UtcNow,
                 }
-            )   ;
+            );
+
+            UfSeeds.Ufs(modelBuilder);
         }
     }
 }
