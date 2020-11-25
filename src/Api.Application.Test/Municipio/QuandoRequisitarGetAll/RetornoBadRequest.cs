5b24607a16ebf69eeff1c3c3c90b35634a922030
Andre <9000andre@gmail.com>
2020-11-25 10:53:31 -0300

    Create GetAll test (application.test/GetAll)

---
+++ src/Api.Application.Test/Municipio/QuandoRequisitarGetAll/RetornoBadRequest.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using Moq;
+using System;
+using Xunit;
+using System.Threading.Tasks;
+using Api.Domain.Interfaces.Services.Municipio;
+using Microsoft.AspNetCore.Mvc;
+using Api.Domain.Dto.Municipio;
+using Api.Application.Controllers;
+namespace Api.Application.Test.Municipio.QuandoRequisitarGetAll
+{
+    public class RetornoBadRequest : ControllerBase
+    {
+        private MunicipiosController _controller;
+
+         [Fact(DisplayName="OkRetorno")]
+         public async Task OkRetorno()
+         {
+             var serviceMock = new Mock<IMunicipioService>();
+             serviceMock.Setup(m=>m.GetAll())
+             .ReturnsAsync(
+                 new List <MunicipioDto>
+                 {
+                     new MunicipioDto
+                     {
+                     Id = Guid.NewGuid(),
+                     Nome = "São Paulo",
+                     },
+                     new MunicipioDto
+                     {
+                         Id = Guid.NewGuid(),
+                         Nome = "Limeira,"
+                     }
+                 }
+             );
+
+             _controller = new MunicipiosController(serviceMock.Object);
+             _controller.ModelState.AddModelError("Id", "Formato invalido");
+
+             var result = await  _controller.GetAll();
+             Assert.True(result is BadRequestObjectResult);
+         }
+    }
+}
